5702aa171352b285bb7ac073b032c00c912c51a4
mr_varshney2004 <vivekvarshney2004@gmail.com>
2023-09-11 22:56:15 +0530

    Added slide

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+/* Slider */
+
+var slide = document.getElementById("slider");
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+var btn4= document.getElementById("btn4");
+
+btn1.onclick = function(){
+    slide.style.transform = "translateX(0px)";
+    btn1.classList.add("active");
+    btn2.classList.remove("active");
+    btn3.classList.remove("active");
+    btn4.classList.remove("active");
+}
+btn2.onclick = function(){
+    slide.style.transform = "translateX(-100%)";
+    btn2.classList.add("active");
+    btn1.classList.remove("active");
+    btn3.classList.remove("active");
+    btn4.classList.remove("active");
+}
+
+btn3.onclick = function(){
+    slide.style.transform = "translateX(-200%)";
+    btn3.classList.add("active");
+    btn1.classList.remove("active");
+    btn2.classList.remove("active");
+    btn4.classList.remove("active");
+}
+
+btn4.onclick = function(){
+    slide.style.transform = "translateX(-300%)";
+    btn4.classList.add("active");
+    btn1.classList.remove("active");
+    btn2.classList.remove("active");
+    btn3.classList.remove("active");
+} 
+
 /* Feedback script */
 const btn = document.querySelector("button");
 const post = document.querySelector(".post");
