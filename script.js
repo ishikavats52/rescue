39835906edffc789780adcb3711d502b9da85021
mr_varshney2004 <vivekvarshney2004@gmail.com>
2023-09-21 11:38:16 +0530

    feedback script change

--- script.js
+++ script.js
@@ -93,51 +93,5 @@ btn.onclick =()=>{
 } */
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDFo6ChvrJ4nvumanLz03xkW_R9XRuDKH8",
-  authDomain: "rescuer-93f65.firebaseapp.com",
-  databaseURL: "https://rescuer-93f65-default-rtdb.firebaseio.com",
-  projectId: "rescuer-93f65",
-  storageBucket: "rescuer-93f65.appspot.com",
-  messagingSenderId: "773889871752",
-  appId: "1:773889871752:web:d3025f181688f42f07743d"
-};
 
-
-firebase.initializeApp(firebaseConfig);
-
-
-var feedbackFormDB = firebase.database().ref("feedback");
-
-document.getElementById("feedbackForm").addEventListener("submit", submitForm);
-
-function submitForm(e) {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  const rate = formData.get('rate');
-  var msgContent = getElementVal("msgContent");
-  saveMessages(rate, msgContent);
-
-  document.querySelector(".alert").style.display = "block";
-
-  setTimeout(() => {
-    document.querySelector(".alert").style.display = "none";
-  }, 1000);
-
-  document.getElementById("feedbackForm").reset();
-
-}
-
-const saveMessages = (rate, msgContent) => {
-  var newfeedbackForm = feedbackFormDB.push();
-
-  newfeedbackForm.set({
-    rate: rate,
-    msgContent: msgContent,
-  });
-};
-
-const getElementVal = (id) => {
-  return document.getElementById(id).value;
-};
 
